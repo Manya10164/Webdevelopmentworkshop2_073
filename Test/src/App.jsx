b14ebe49e3a8397b5f2f116e8c5bc6cd28ffca36
Manya10164 <shigh0108@gmail.com>
2026-05-13 11:20:02 +0530

    reactt

---
+++ Test/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sample from './Components/Sample'
+import Sample2 from './Components/Sample2'
+
+const App = () => {
+
+
+  return (
+    // <div>App</div>
+    // <Sample/>
+    <Sample2/>
+  )
+}
+
+export default App
---
+++ Test/src/Components/Sample.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+const Sample = () => {
+  return (
+    <// App.js
+
+import React from "react";
+import "./App.css";
+
+function ProductCard({ name, price, category, bgColor, borderColor, textColor }) {
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundColor: bgColor,
+        border: `4px solid ${borderColor}`,
+        color: textColor,
+      }}
+    >
+      <h2>{name}</h2>
+      <p>
+        <b>Price:</b> ₹{price}
+      </p>
+      <p>
+        <b>Category:</b> {category}
+      </p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Product Showcase</h1>
+
+      <div className="top-row">
+        <ProductCard
+          name="Wireless Mouse"
+          price="799"
+          category="Electronics"
+          bgColor="#b9dff1"
+          borderColor="blue"
+          textColor="darkblue"
+        />
+
+        <ProductCard
+          name="Running Shoes"
+          price="2499"
+          category="Footwear"
+          bgColor="#98f28d"
+          borderColor="green"
+          textColor="darkgreen"
+        />
+      </div>
+
+      <div className="bottom-row">
+        <ProductCard
+          name="Smart Watch"
+          price="3999"
+          category="Gadgets"
+          bgColor="#f8b6c0"
+          borderColor="red"
+          textColor="darkred"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;>
+    <div>Sample</div>
+   name <input type="text" />
+    </>
+  )
+}
+
+export default Sample
